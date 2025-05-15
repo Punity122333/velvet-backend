59fabe3ae86cf44d89d5f87bfd33edf340f8bb51
Punity122333 <parthiv.pal.theheritageschool@gmail.com>
2025-05-15 14:14:48 +0530

    Fixed 42PO5 Already Prepared Error

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use sqlx::{postgres::PgPoolOptions, Row};
 use dotenv::dotenv;
+use sqlx::{postgres::PgConnectOptions, Pool, Postgres, Row};
 use std::env;
 
 fn sanitize_sql_input(input: &str) -> String {
@@ -28,33 +28,41 @@ fn sanitize_sql_input(input: &str) -> String {
     sanitized_input
 }
 
-#[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
+async fn supabase_test(pool: &Pool<Postgres>) -> i32 {
+    let rows = sqlx::query("SELECT 1 AS value")
+        .bind(sanitize_sql_input("SELECT 1 AS value")) 
+        .fetch_all(pool)
+        .await
+        .expect("Failed to execute query");
+    let row = rows.into_iter().next().expect("No rows returned");
+    let value: i32 = row.try_get("value").expect("Failed to get value");
+
+    value
+}
+async fn init_env() -> Pool<Postgres> {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
     println!("DATABASE_URL: {}", database_url);
 
-    let connect_options = sqlx::postgres::PgConnectOptions::new()
-        .host("aws-0-ap-southeast-1.pooler.supabase.com")
-        .port(6543)
-        .username("postgres.turmgklvnzdtzpupiojs")
-        .password("Noire2007")
-        .database("postgres")
+    let options = database_url
+        .parse::<PgConnectOptions>()
+        .expect("Invalid database URL")
         .statement_cache_capacity(0);
 
-    println!("Connecting with user: {:?}", connect_options.get_username());
+    sqlx::postgres::PgPoolOptions::new()
+        .max_connections(5)
+        .connect_with(options)
+        .await
+        .expect("Failed to create connection pool")
+}
 
-    let pool = PgPoolOptions::new()
-        .max_connections(10)
-        .connect_with(connect_options)
-        .await?;
+#[tokio::main]
+async fn main() {
+    let pool = init_env().await;
 
-    let row = sqlx::raw_sql(&sanitize_sql_input("SELECT 1 as value"))
-        .fetch_one(&pool)
-        .await?;
-
-    let value: i32 = row.try_get("value")?;
-
+    let value = supabase_test(&pool).await;
     println!("Supabase says: {}", value);
-    Ok(())
+
+    let value = supabase_test(&pool).await;
+    println!("Supabase says: {}", value);
 }
