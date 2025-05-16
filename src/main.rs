7ec1dd8866efb94e93db2845b88d415a4b8c058b
Punity122333 <parthiv.pal.theheritageschool@gmail.com>
2025-05-16 18:08:26 +0530

    Finally am able to POST request shit to Supabase lesgo

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,36 @@ mod state;
 mod routes;
 mod controllers;
 
-use axum::{Router, routing::post};
+use axum::{response::IntoResponse, routing::{get, post}, Router};
 use dotenv::dotenv;
 use state::AppState;
 use routes::admin::add_product;
 use tokio::net::TcpListener;
+use tower_http::cors::{Any, CorsLayer};
+
+
+
+
+async fn root_handler() -> impl IntoResponse {
+    "Hello, world!"
+}
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
 
     let db = db::connect_db().await;
     let state = AppState { db: db.into() };
 
     let app = Router::new()
         .route("/admin/products/:category", post(add_product))
+        .route("/", get(root_handler))
+        .layer(cors)
         .with_state(state);
 
     let addr = "0.0.0.0:3000";
